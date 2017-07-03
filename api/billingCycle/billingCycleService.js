b68df24b068eb0ac011f5434db99fe6d25a79ea6
brunoromeiro <bruno.romeiro@gmail.com>
2017-07-03 16:06:20 -0300

    Atualizando com resolução de erros.
    Atualizando com resolução de erros.

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']) //node-restuful cria todos os servicos rest
@@ -8,6 +9,24 @@ BillingCycle.methods(['get', 'post', 'put', 'delete']) //node-restuful cria todo
 BillingCycle.updateOptions({new: true, runValidators: true})
 //new sempre mostrar o novo
 //runValidators valida a cada put
+
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+function sendErrorsOrNext(req, res, next){
+  const bundle = res.locals.bundle
+
+  if(bundle.errors){
+    var errors = parseErrors(bundle.errors)
+    res.status(500).json({errors})
+  } else {
+    next()
+  }
+}
+
+function parseErrors(nodeRestfulErrors){
+  const errors = []
+  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+  return errors
+}
 
 BillingCycle.route('count', function(req, res, next){
   BillingCycle.count(function(error, value){
--- api/billingSummary/billingSummaryService.js
+++ api/billingSummary/billingSummaryService.js
@@ -1,4 +1,4 @@
-const _ =require('lodash')
+const _ = require('lodash')
 const BillingCycle = require('../billingCycle/billingCycle')
 
 //Mais uma função middleware
